e6fd9d3b97d396f2c4e654d0c99fa3ce855a9942
jerry <jiangyuanlucky@gmail.com>
2013-06-13 22:24:51 +0800

    update route.js server.js...add bind.html nth-child.html...

--- route.js
+++ route.js
@@ -1,48 +1,38 @@
 var fs = require( "fs" );
+var path = require("path");
 
 
-exports.parse = function( url, res ) {
+exports.parse = function( url ) {
     var content;
     console.log( "pasre start" );
     if ( url === "/" ) {
-        parseContent( "./index.html", res );
-        return;
+        return parseContent( "./index.html" );
     }
 
     if ( /\.(?:js|css|htm|html)$/i.test( url ) ) {
-        parseContent( "." + url, res );
-        return;
+        return parseContent( "." + url );
+    }
+
+    if ( /\.(?:gif|jpg|png)$/i.test( url ) ) {
+        return parseBuffer( "." + url );
     }
 
 }
 
-function parseContent( url, res ) {
-    fs.readFile( url, function( error, content ) {
-        if ( error ) {
-            res.end( "parse error" );
-        } else {
-            var type = getType( url );
-            res.writeHead( 200, "response succussfully", {
-                "Content-Length": content.length,
-                "Content-Type": type || "text/plain"
-            });
-            res.end( content );
-            console.log( "response succussfully" );
-        }
-    });
-}
-
-function getType( url ) {
-    var def = "text/plain";
-    if ( !url ) return def;
-    var rType = /\.([a-z]+)/i;
-    console.log( url );
-    var type = {
-        js: "application/javascript",
-        css: "text/css",
-        html: "text/html",
-        htm: "text/htm"
+function parseContent( url ) {
+    if ( fs.existsSync(url) ) {
+        return fs.readFileSync( url, "utf-8" );
+    } else {
+        return null;
     }
-
-    return type[ rType.exec(url)[1] ] || def;
+    
 }
+
+function parseBuffer( url ) {
+    if ( fs.existsSync(url) ) {
+        return fs.readFileSync( url );
+    } else {
+        return null;
+    }
+}
+
--- server.js
+++ server.js
@@ -5,9 +5,51 @@ var port = process.argv[2] || 3000;
 
 http.createServer(function( req, res ) {
     var url = req.url;
+    var content = route.parse( url );
+    var length;
+    var type = getType( url );
 
-    route.parse( url, res );
+    if ( !content ) {
+        console.log( "no " + url );
+        length = 0;
+    } else {
+        length = content.length;
+    }
+
+
+
+    res.writeHead( 200, "response succussfully", {
+        "Content-Length": length,
+        "Content-Type": type || "text/plain"
+    });
+    if ( typeof content === "string" ) {
+        res.end( content );
+    } else {
+        res.end( content, "binary" );
+    }
+    
+    console.log( "response succussfully" );
     
 }).listen( port );
 
 console.log( "server listening at " + port );
+
+function getType( url ) {
+    if ( url === "/" ) {
+        return "text/html";
+    }
+    var def = "text/plain";
+    if ( !url ) return def;
+    var rType = /\.([a-z]+)/i;
+    var type = {
+        js: "application/javascript",
+        css: "text/css",
+        html: "text/html",
+        htm: "text/htm",
+        jpg: "image/jpg",
+        gif: "image/gif",
+        png: "image/png"
+    }
+    return type[ rType.exec(url)[1].toLowerCase() ] || def;
+}
+
