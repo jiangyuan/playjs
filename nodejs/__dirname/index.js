43fd1b7de9c355b504333ee5912bbfc29e622c23
jiangyuan <jiangyuanlucky@gmail.com>
2014-09-30 19:39:29 +0800

    nodejs

---
+++ nodejs/__dirname/index.js
@@ -0,0 +1,6 @@
+console.log(__dirname); //  xx/xxx/__dirname
+console.log('---');
+require('./dir/test');   //  xx/xxx/__dirname/dir
+
+
+// __dirname is always the directory of the file in which is used.
